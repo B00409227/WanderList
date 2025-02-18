5fdee150070e0d4fd632f8d720b22e88b85cb859
B00409227 <127535780+B00409227@users.noreply.github.com>
2025-02-18 00:38:39 +0000

    Initial commit - Restarted Git repository

---
+++ wanderlist/WanderList-TWA/app/src/main/java/app/web/wanderlist_59fb6/twa/DelegationService.java
@@ -0,0 +1,18 @@
+package app.web.wanderlist_59fb6.twa;
+
+
+import com.google.androidbrowserhelper.locationdelegation.LocationDelegationExtraCommandHandler;
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+            registerExtraCommandHandler(new LocationDelegationExtraCommandHandler());
+        
+    }
+}
+
